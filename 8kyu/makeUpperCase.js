4efa4bae06eb68a3852ae573fcb6cf3f34956f1e
EusebiuzPR <273243314+EusebiuzPR@users.noreply.github.com>
2026-04-27 22:00:29 -0500

    .toUpperCase() exercise

---
+++ 8kyu/makeUpperCase.js
@@ -0,0 +1,12 @@
+/* Description 
+
+Write a function which converts the input string to uppercase.
+*/
+
+// Solution 
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase();
+}
+console.log(makeUpperCase("javascript <3"));
